8aa21acff21791211f2a95d93209d9c3ba04160d
Peter Adjao <pa376499@gmail.com>
2025-08-19 21:57:18 +0000

    feat: add Directors interface which extends Teacher interface with require

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -23,5 +23,20 @@ const teacher2: Teacher = {
     contract: false
 };
 
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+} 
+
+const director1: Directors = {
+    firstName: "John",
+    lastName: "Doe",
+    location: "London",
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+}
+
+console.log(director1);
+
 //step 3: print teacher2
 console.log(teacher2);
