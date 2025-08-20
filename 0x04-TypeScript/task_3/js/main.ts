924bc5efc8483d034038c22aa6288e9032214918
Peter Adjao <pa376499@gmail.com>
2025-08-20 22:20:09 +0000

    fix: Resolve variable declarations in main.ts to match checker requirement

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -12,9 +12,7 @@ const row: RowElement = {
 const newRowID: RowID = CRUD.insertRow(row);
 
 //Step 3: Update the row with age
-const updatedRow: RowElement = { ...row, age: 23
-
-};
+const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 
 //Step 4:  Delete the row
