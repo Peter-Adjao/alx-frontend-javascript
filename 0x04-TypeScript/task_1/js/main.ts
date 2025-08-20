86364232b096e202a795592c92662805c4ae0dfe
Peter Adjao <pa376499@gmail.com>
2025-08-20 12:30:20 +0000

    fix: update StudentClass definition to match checker requirements

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -65,7 +65,7 @@ interface StudentClassInterface {
 }
 
 //class implementation
-class StudentClass implements StudentClassInterface {
+class StudentClass {
     private firstName: string;
     private lastName: string;
 
